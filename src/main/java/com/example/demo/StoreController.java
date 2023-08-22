572de34c4181d8dcdd699ccf90d4aa472849c562
dabi <dabich626626@gmail.com>
2023-08-22 16:55:12 +0200

    ver1

---
+++ src/main/java/com/example/demo/StoreController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class StoreController {
+
+    @RequestMapping("/store/order/add")
+    public Product add(@RequestParam int ProductId){
+
+        return ProductId();
+    }
+
+    @RequestMapping("/store/order/get")
+    public Product get(@RequestParam int ProductId){
+
+        return ProductId();
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/StoreService.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+@Service
+@Configuration
+public class StoreService {
+
+
